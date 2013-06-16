eba7c35c4839745cd0a1ddf60ab5b9342a6b63e9
Eduardo Elias Camponez - Notebook <camponez@gmail.com>
2013-06-16 16:25:31 -0300

    Initial commit

---
+++ escala.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+print "Content-type: text/html\n\n"
+
+import xml.etree.ElementTree as ET
+from datetime import datetime
+from datetime import time as dtime
+import time
+import os
+
+class Escala:
+    def __init__(self, arquivo_xml):
+        self.escalas = []
+
+        root = self.__load_xml(arquivo_xml)
+        self.__parser(root)
+
+    def __load_xml(self, arquivo_xml):
+        if not os.path.exists(arquivo_xml):
+           print 'Oh dear.'
+           raise
+
+        tree = ET.parse(arquivo_xml)
+        root = tree.getroot()
+
+        return root
+
+    def __parser(self, root):
+        for child in root:
+            voo = Voo()
+
+            datahora =  time.strptime(child[11].text, "%d/%m/%Y %H:%M:%S")
+            voo.activity_date = datetime.fromtimestamp(time.mktime(datahora))
+            voo.present_location = child[8].text
+            #decolagem
+            voo.sta = dtime(int(child[16].text[:-3]),
+                        int(child[16].text[3:]))
+
+            #pouso
+            voo.std = dtime(int(child[15].text[:-3]),
+                        int(child[15].text[3:]))
+            voo.actype = child[17].text
+            voo.flight_no = child[18].text
+            voo.origin = child[19].text
+            voo.destination = child[20].text
+            voo.activity_info = child[22].text
+
+            if child[9].text is not None:
+                voo.checkin = True
+                voo.checkin_time = dtime(int(child[9].text[:-3]),
+                        int(child[9].text[3:]))
+            else:
+                voo.checkin = False
+                voo.checkin_time = None
+
+            self.escalas.append(voo)
+
+    def csv(self):
+        csv = 'Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description\n'
+
+        for voo in self.escalas:
+            if voo.activity_info == 'FR':
+                csv += 'FR,'
+                csv += voo.activity_date.strftime('%m/%d/%Y')+","
+                csv += voo.sta.strftime('%H:%M')+","
+                csv+=voo.activity_date.strftime('%m/%d/%Y')+","
+                csv += voo.std.strftime('%H:%M')+","
+                csv+='True,-\n'
+                continue
+
+            if voo.activity_info == 'REU':
+                csv += 'REU,'
+                csv += voo.activity_date.strftime('%m/%d/%Y')+","
+                csv += voo.sta.strftime('%H:%M')+","
+                csv+=voo.activity_date.strftime('%m/%d/%Y')+","
+                csv += voo.std.strftime('%H:%M')+","
+                csv+='False,-\n'
+                continue
+
+            if voo.checkin:
+                csv += 'Checkin,'
+                #Data hora inicial
+                csv += voo.activity_date.strftime('%m/%d/%Y')+","
+                csv += voo.checkin_time.strftime('%H:%M')+","
+                csv += voo.activity_date.strftime('%m/%d/%Y')+","
+                csv += voo.checkin_time.strftime('%H:%M')+","
+                csv+='False,-\n'
+
+            csv+='Voo '+voo.origin+'-'+voo.destination+' ('+voo.activity_info+'),'
+            csv += voo.activity_date.strftime('%m/%d/%Y')+","
+            csv += voo.sta.strftime('%H:%M')+","
+            csv+=voo.activity_date.strftime('%m/%d/%Y')+","
+            csv += voo.std.strftime('%H:%M')+","
+
+            csv+='False,-'
+
+            csv += '\n'
+
+
+        return csv
+
+    def get_numero_voos(self):
+        return len(self.escalas)
+
+class Voo:
+    def __init__(self):
+        self.activity_date = None
+        self.presentLocation = None
+        self.flight_no = None
+        self.origin = None
+        self.destination = None
+        self.actype = None
+        self.checkin = None
+        self.checkin_time = None
+        self.std = None
+        self.sta = None
+        self.activity_info = None
+
+if __name__ == "__main__":
+    print "<html><body><pre>"
+    try:
+        escala = Escala('escala.xml')
+        output = escala.csv()
+
+        f = open('tmp/escala.csv', 'w+')
+        f.write(output)
+        f.close()
+
+        print output
+    except:
+        import sys
+        print "Unexpected error:", sys.exc_info()[1]
+
+    print "</pre>"
+    print "<a href='tmp/escala.csv'>escala.csv</a>"
+
+    print "</body></html>"
---
+++ tests/test_escala.py
@@ -0,0 +1,61 @@
+import unittest
+import sys
+from datetime import datetime
+from datetime import time
+
+sys.path.insert(0, '../')
+
+from escala import Escala
+
+
+class FrameTest(unittest.TestCase):
+
+    def setUp(self):
+        self.escala = Escala('./fixtures/escala.xml')
+
+    def tearDown(self):
+        pass
+
+    def test_AttributosPrimeiroVoo(self):
+        p_voo = self.escala.escalas[0]
+
+        self.assertEqual(p_voo.activity_date,datetime(2013,3,1,14,36))
+        self.assertEqual(p_voo.present_location, 'VCP')
+        self.assertEqual(p_voo.flight_no, '4148')
+        self.assertEqual(p_voo.origin, 'VCP')
+        self.assertEqual(p_voo.destination, 'GYN')
+        self.assertEqual(p_voo.actype, 'E95')
+        self.assertTrue(p_voo.checkin)
+        self.assertEqual(p_voo.checkin_time, time(10,36))
+        self.assertEqual(p_voo.std, time(13,13))
+        self.assertEqual(p_voo.sta, time(11,36))
+        self.assertEqual(p_voo.activity_info, 'AD4148')
+
+    def test_AttributosSegundoVoo(self):
+        p_voo = self.escala.escalas[1]
+        self.assertFalse(p_voo.checkin)
+        self.assertEqual(p_voo.checkin_time, None)
+        self.assertEqual(p_voo.activity_info, 'AD4148')
+
+    def test_AttributosQuartoVoo(self):
+        p_voo = self.escala.escalas[4]
+        self.assertFalse(p_voo.checkin)
+        self.assertEqual(p_voo.checkin_time, None)
+        self.assertEqual(p_voo.flight_no,None)
+        self.assertEqual(p_voo.activity_info, 'FR')
+
+    def test_DiaSemVoo(self):
+        p_voo = self.escala.escalas[3]
+
+    def test_NumerosVoos(self):
+        self.assertEqual(self.escala.get_numero_voos(),6)
+
+    def test_CSV(self):
+        self.assertEqual(self.escala.csv(),'Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description\nCheckin,03/01/2013,10:36,03/01/2013,10:36,False,-\nVoo VCP-GYN (AD4148),03/01/2013,11:36,03/01/2013,13:13,False,-\nVoo GYN-PMW (AD4148),03/01/2013,13:55,03/01/2013,15:15,False,-\nCheckin,04/01/2013,12:28,04/01/2013,12:28,False,-\nVoo CNF-VCP (AD4049),04/01/2013,13:13,04/01/2013,14:28,False,-\nFR,04/02/2013,03:15,04/02/2013,03:15,True,-\nFR,04/03/2013,03:15,04/03/2013,03:15,True,-\nREU,04/08/2013,13:40,04/08/2013,17:00,False,-\n')
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
