f759ba2144200c97097f0578de31cadc774b02ef
Eduardo Elias Ferreira <camponez@gmail.com>
2013-08-19 11:15:12 -0300

    - geracao e calculo para dias diferente - mudança nos numeros de voos

--- tests/test_escala.py
+++ tests/test_escala.py
@@ -35,7 +35,7 @@ class FrameTest(unittest.TestCase):
         p_voo = self.escala.escalas[1]
         self.assertFalse(p_voo.checkin)
         self.assertEqual(p_voo.checkin_time, None)
-        self.assertEqual(p_voo.activity_info, 'AD4148')
+        self.assertEqual(p_voo.activity_info, 'AD4298')
 
     def test_AttributosQuartoVoo(self):
         p_voo = self.escala.escalas[4]
@@ -55,13 +55,13 @@ class FrameTest(unittest.TestCase):
 Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description\n\
 Checkin,03/01/2013,10:36,03/01/2013,10:36,False,-\n\
 Flight AD4148 VCP-GYN,03/01/2013,11:36,03/01/2013,13:13,False,-\n\
-Flight AD4148 GYN-PMW,03/01/2013,13:55,03/01/2013,15:15,False,-\n\
+Flight AD4298 GYN-PMW,03/01/2013,23:45,03/02/2013,01:55,False,-\n\
 Checkin,04/01/2013,12:28,04/01/2013,12:28,False,-\n\
 Flight AD4049 CNF-VCP (E),04/01/2013,13:13,04/01/2013,14:28,False,-\n\
 FR,04/02/2013,03:15,04/02/2013,03:15,True,-\n\
 FR,04/03/2013,03:15,04/03/2013,03:15,True,-\n\
 REU,04/08/2013,13:40,04/08/2013,17:00,False,-\n\
-Flight AD4148 GYN-PMW,08/05/2013,21:15,08/05/2013,22:55,False,-\n\
+Flight AD4035 GYN-PMW,08/05/2013,21:15,08/05/2013,22:55,False,-\n\
 Reserva,09/04/2013,08:16,09/04/2013,14:15,False,-\n\
 Reserva,09/05/2013,04:46,09/05/2013,08:45,False,-\n\
 SobAviso,09/08/2013,11:00,09/08/2013,23:00,False,-\n'
@@ -69,7 +69,7 @@ SobAviso,09/08/2013,11:00,09/08/2013,23:00,False,-\n'
         self.assertEqual(self.escala.csv(), check_output)
 
     def test_CalculoHorasVoadas(self):
-        self.assertEqual(self.escala.somaHoras(), 277)
+        self.assertEqual(self.escala.somaHoras(), 327)
 
 def main():
     unittest.main()
