434ec8126e9fa206886df2046298e04d377e4ec8
Eduardo Elias Ferreira <camponez@gmail.com>
2013-09-04 17:18:37 -0300

    V1.1

--- escala.py
+++ escala.py
@@ -205,7 +205,7 @@ class Voo:
 
 if __name__ == "__main__":
     print "<html><body>"
-    print "<span>V1.0</span>"
+    print "<span>V1.1</span>"
     print '<form action="escala.py" method="post" enctype="multipart/form-data">'
     print 'Upload file: <input type="file" name="myfile" /> <br />'
     print ' <input type="submit" name="submit" value="Submit" />'
