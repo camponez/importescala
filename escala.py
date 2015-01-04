a4dc17349300b5ff55535ed58e65b88854993e32
Eduardo Elias <camponez@gmail.com>
2015-01-04 11:10:52 -0200

    Add analytics

--- escala.py
+++ escala.py
@@ -380,7 +380,19 @@ if __name__ == "__main__":
         print traceback.format_exc()
 
 
+    ANALYTICS = "\
+<script>\
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\
+ga('create', 'UA-2271271-13', 'auto');\
+ga('send', 'pageview');\
+</script>\
+"
 
+
+    print ANALYTICS
     print "</body></html>"
 
 # vim:tabstop=4:expandtab:smartindent
